806dc3483373ec22501df7801b9c21d6a26543c9
mynul-islam-madhurjo <iammynul.bracu@gmail.com>
2023-08-09 08:02:17 +0600

    fresh commit

---
+++ 217._Contains_Duplicate.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+    for(let i=0; i<nums.length;i++) {
+        for(let j = 0; j<nums.length;j++){
+            if (a)
+        }
+    }
+};
+
+a = [1,2,1];
+
+console.log(containsDuplicate(a))
