0f4cd538e9de7a878eeac25af611394c5660f38f
mynul-islam-madhurjo <iammynul.bracu@gmail.com>
2023-08-14 11:06:15 +0600

    contains duplicate solution

--- 217.Contains_Duplicate.js
+++ 217.Contains_Duplicate.js
@@ -3,14 +3,59 @@
  * @return {boolean}
  */
 var containsDuplicate = function(nums) {
-    for(let i=0; i<nums.length;i++){
-        const newNum = nums.slice(i+1,nums.length);
-        if (newNum.includes(nums[i])){
-            return true;
-        }else{
-            return false;
+    if(1<=nums.length<=100000){
+        for(let i=0; i<nums.length;i++){
+            const num = nums[i];
+            const newNum = nums.slice(i+1,nums.length);
+            if (-1000000000 <= num <= 1000000000){
+                if (newNum.includes(num)){
+                    return true;
+                }
+            }else{
+                return false
+            }
         }
     }
+    return false;
 };
 
-console.log(containsDuplicate([1,2,1]))
+var opcontainsDuplicate = function(nums) {
+    let newArr = [];
+    if(1 <= nums.length && nums.length <= 100000){
+        for(let i=0; i<nums.length;i++){
+            const num = nums[i];
+            console.log(newArr)
+            if (-1000000000 <= num && num <= 1000000000){
+                if (newArr.includes(num)){
+                    return true;
+                }else{
+                    newArr.push(num);
+                }
+            }
+        }
+    }
+    return false;
+};
+
+var opxcontainsDuplicate = function(nums) {
+    let numSet = new Set();
+
+    if (1 <= nums.length && nums.length <= 100000) {
+        for (let i = 0; i < nums.length; i++) {
+            const num = nums[i];
+            if (-1000000000 <= num && num <= 1000000000) {
+                if (numSet.has(num)) {
+                    return true;
+                } else {
+                    numSet.add(num);
+                }
+            } else {
+                return false;
+            }
+        }
+    }
+
+    return false;
+};
+
+console.log(opxcontainsDuplicate([2,14,18,22,22]))
